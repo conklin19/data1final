41bfa0642dbd399c179be83051bd0ba0615b1637
conklin19 <conklin19@gmail.com>
2022-11-13 17:12:24 -0500

    find highest rated F4 team

--- conkfinal.py
+++ conkfinal.py
@@ -3,14 +3,18 @@
 ## Adam Conklin ##
 ## Analyzing KenPom Data for NCAA Final Four Teams ##
 
+import matplotlib as mp
 import pandas as pd
 import seaborn as sns
-import matplotlib as mp
 
 df = pd.read_csv('assets\KPdata.csv')
 
 data = df
-data = data[data.Year != 2020]
-print(data)
-
-print(df)
+##Requirement 2.1 remove null values ###
+##Delete 2020 because NO TOURNAMENT due to COVID-19##
+###Delete the 4 rows where the 2020 Final Four teams woud have been###
+data = data[data.Year != 2020]  
+avg = data.KPvalue.mean()
+print(avg)
+maxKPvalue = data[['KPvalue']].max()
+print(maxKPvalue)
